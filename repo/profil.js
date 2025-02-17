fe5d0da769273b33c44ab2160b922a4390a898da
langowanprojecttesting <langowanproject.testing@outlook.co.id>
2025-02-17 22:27:31 +0800

    Update profil.js

--- repo/profil.js
+++ repo/profil.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
         <h3>Bride & Groom</h3>
         <div class="card-wrap">
             <div class="card1 top">
-                <h4 class="judul-quotes1"></h4>
                 <p class="quotes1"></p>
                 <h5 class="sumber-quotes1"></h5>
             </div>
@@ -14,25 +13,24 @@ document.addEventListener("DOMContentLoaded", function () {
             <div class="card4 left">
                 <div class="box bride-image"></div>
                 <h4 class="bride"></h4>
-                <p class="biodata-bride"></p>
+                <p>Anak <span class="anak-ke"></span> Bapak <span class="ayah"></span> dan Ibu <span class="ibu"></span></p>
                 <div class="item-acara">
-                    <span><h5 class="icon">c</h5></span>
-                    <h5 class="alamat-bride"></h5>
+                    <span><h5 class="icon">c</h5></span><br>
+                    <h5 class="alamat-groom"></h5>
                 </div>
             </div>
             <div class="card5 right">
                 <div class="box groom-image"></div>
                 <h4 class="groom"></h4>
-                <p class="biodata-groom"></p>
+                <p>Anak <span class="anak-ke"></span> bapak <span class="ayah"></span> dan ibu <span class="ibu"></span></p>
                 <div class="item-acara">
-                    <span><h5 class="icon">c</h5></span>
+                    <span><h5 class="icon">c</h5></span><br>
                     <h5 class="alamat-groom"></h5>
                 </div>
             </div>
         </div>
         <div class="card-wrap">
             <div class="card2 bottom">
-                <h4 class="judul-quotes2"></h4>
                 <p class="quotes2"></p>
                 <h5 class="sumber-quotes2"></h5>
             </div>
